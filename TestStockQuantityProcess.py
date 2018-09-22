63db9c06048f885bd64eb703497d03e09b1a2636
RajatChaudhari <silverprince.v1@gmail.com>
2018-09-22 11:05:25 +0530

    changing file name and formatting

--- TestStockQuantityProcess.py
+++ TestStockQuantityProcess.py
@@ -13,34 +13,36 @@ import os
 
 class TestStockQuantityProcess(unittest.TestCase):
     
-    filepath=os.getcwd()
+    filepath = os.getcwd()
 
     def setup(self):
         '''takes the expected file and caculated end of day position file'''
         
-        expected_pos=input("Enter expected position file name along with extention : ")
-        calculated_pos=input("Enter start positions file name along with extention : ")
+        expected_pos = input("Enter expected position file name along with extention : ")
+        calculated_pos = input("Enter start positions file name along with extention : ")
         
         try:
-            expected_positions=pd.read_csv(self.filepath+"/"+expected_pos)
+            expected_positions = pd.read_csv(self.filepath + "/files/" + expected_pos)
         except Exception as e:
             print("Error in handling starting postion file: {}".format(e))
-            expected_positions=None
+            expected_positions = None
         try:
-            calculated_positions=pd.read_csv(self.filepath+"/"+calculated_pos)
+            calculated_positions = pd.read_csv(self.filepath + "/output/" + calculated_pos)
         except Exception as e:
             print("Error in handling starting postion file: {}".format(e))
-            calculated_positions=None
+            calculated_positions = None
         
         if expected_positions is not None and calculated_positions is not None:
-            self.test_recordsmatch(expected_positions,calculated_positions)
+            self.test_recordsmatch(expected_positions, calculated_positions)
+            self.test_recordsdontmatch(expected_positions, calculated_positions)
                 
-    def test_recordsmatch(self,expected_positions,calculated_positions):        
-        self.assertDictEqual(expected_positions.to_dict(),calculated_positions.to_dict(),"Check the Process Logic")
+    def test_recordsmatch(self, expected_positions,calculated_positions):        
+        self.assertDictEqual(expected_positions.to_dict(), calculated_positions.to_dict(), "Check the Process Logic")
     
-    def test_recordsdontmatch(self,expected_positions,calculated_positions):        
-        self.assertEqual(expected_positions.iloc[1][3],calculated_positions.iloc[0][3],"This was expected as we are comapring values from different rows")
-if __name__=="__main__":
+    def test_recordsdontmatch(self, expected_positions,calculated_positions):        
+        self.assertEqual(expected_positions.iloc[1][3], calculated_positions.iloc[0][3], "This was expected as we are comapring values from different rows")
+
+if __name__ == "__main__":
     
     a=TestStockQuantityProcess()
     a.setup()
